6e2fbeb0c8513193bddb561fc7205be92ecf7d4e
Akshay Bajaj <akshay.bajaj@marklogic.com>
2021-04-30 13:04:27 -0700

    Added

--- marklogic-data-hub-central/ui/src/components/entities/matching/compare-values-modal/compare-values-modal.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/matching/compare-values-modal/compare-values-modal.tsx
@@ -1,31 +1,86 @@
-import React from "react";
-import {Modal} from "antd";
+import React, {useState} from "react";
+import {Modal, Table} from "antd";
+import "./compare-values-modal.scss"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import styles from "./compare-values-modal.module.scss";
 import {MLTable} from "@marklogic/design-system";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
    isVisible: any;
    toggleModal: (isVisible: boolean) => void;
+   uriInfo: any;
+   activeStepDetails: any;
+   entityProperties:any
 }
 
 const CompareValuesModal: React.FC<Props> = (props) => {
+    let sampleData;
+    let counter=0;
+
     const closeModal = () => {
         props.toggleModal(false);
     }
 
+    const columns = [
+        {
+            title: 'URI1',
+            dataIndex: 'name',
+            key: 'property' + counter++,
+                render: (name,key) => {
+                    return <span key ={key}>{name} </span>
+                }
+        },
+        // {
+        //     title: 'URI2',
+        //     dataIndex: 'property2',
+        //     key: 'property2' + 'uris2' + counter++,
+        //     render: (property2,key) => {
+        //         return <span key ={key}>{property2.fname} </span>
+        //     }
+        // },
+    ];
+
+        if(props.uriInfo != undefined){
+        console.log("uriInfo is ....",props.uriInfo);
+        let info1, info2;
+        let property1, property2;
+        if(props.uriInfo[0].result1 !== undefined){
+            info1 = props.uriInfo[0].result1.data.data.envelope.instance;
+            property1=info1[props.activeStepDetails.entityName]
+            console.log("property 1 ",property1)
+        }
+        if(props.uriInfo[1].result2 !== undefined){
+            info2 = props.uriInfo[1].result2.data.data.envelope.instance;
+            property2=info2[props.activeStepDetails.entityName]
+        }
+        let data = {
+            property1: property1,
+            property2: property2,
+            properties: props.entityProperties,
+            key: counter++,
+        }
+            sampleData = data;
+            console.log("sampleData .....",sampleData); //props.uriInfo[0].result1.data.data.envelope
+    }
     return <Modal
         visible={props.isVisible}
         closable={true}
         maskClosable={false}
         title={null}
-        footer={null}
         width={1400}
         destroyOnClose={true}
         onCancel={closeModal}
         onOk={closeModal}
         >
-        TestData </Modal>
+        <MLTable
+          dataSource={props.entityProperties}
+          columns={columns}
+          rowKey="key"
+          id="compareValuesTable"
+        >
+        </MLTable>
+        </Modal>
 };
 
 export default CompareValuesModal;
